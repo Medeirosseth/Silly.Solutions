9556d32d29bca62d9ff8851beec98784539c16dc
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-28 12:51:46 -0700

    add a controller and a veiw for details

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -42,5 +42,15 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/machines/details/{id}")]
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+      .Include(machine => machine.JoinEntities)
+      .ThenInclude(join => join.Engineer)
+      .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
