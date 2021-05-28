aa556ba6fee54abd4f1ff55ca82c0db6a81dd65f
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-28 11:11:15 -0700

    add machines index route

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers 
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/machines")]
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+  }
+}
